efe6ec3081942e683341341c063924be82453b44
petr.schneider <schnpt@seznam.cz>
2019-08-08 12:14:57 +0200

    FIX app animation

--- app/src/main/java/com/example/schnp/hatappone/MainActivity.java
+++ app/src/main/java/com/example/schnp/hatappone/MainActivity.java
@@ -11,27 +11,30 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity {
 
     ImageView bgapp, clover;
-    Animation frombottom;
     LinearLayout textsplash, texthome, menus;
+    Animation frombottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
 
         bgapp = (ImageView) findViewById(R.id.bgapp);
         clover = (ImageView) findViewById(R.id.clover);
-        textsplash = (LinearLayout) findViewById(R.id.textSplash);
+        textsplash = (LinearLayout) findViewById(R.id.textsplash);
         texthome = (LinearLayout) findViewById(R.id.texthome);
         menus = (LinearLayout) findViewById(R.id.menus);
 
-        bgapp.animate().translationY(-1200).setDuration(700).setStartDelay(400);
-        clover.animate().alpha(0).setDuration(700).setStartDelay(400);
+        bgapp.animate().translationY(-1900).setDuration(800).setStartDelay(300);
+        clover.animate().alpha(0).setDuration(800).setStartDelay(600);
         textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
 
         texthome.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
+
 
     }
 }
